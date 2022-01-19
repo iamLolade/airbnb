597d62e6f018d78856c0dcedae3b235315f69cc9
iamLolade <adeniyiotemade@gmail.com>
2022-01-19 14:02:53 +0100

    Created and styled nav component

---
+++ src/components/Navbar.js
@@ -0,0 +1,12 @@
+import React from "react"
+import logo from "../images/Vector.png"
+
+function Navbar() {
+    return (
+        <nav className="nav">
+            <img src={logo} alt="Brand-logo"/>
+        </nav>
+    )
+}
+
+export default Navbar;
