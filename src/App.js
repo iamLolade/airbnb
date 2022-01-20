e9cad4ddc27f4ee41955dded881f316a716b0860
iamLolade <adeniyiotemade@gmail.com>
2022-01-20 05:12:31 +0100

    Created fake database and implemented dynamic data population

--- src/App.js
+++ src/App.js
@@ -2,12 +2,27 @@ import React from "react"
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import Card from "./components/Card";
+import data from "./data"
 
 const App = () => {
+    const card = data.map(val => {
+      return <Card
+                key={val.id} 
+                img={val.coverImg}
+                rating={val.stats.rating}
+                reviewCount={val.stats.reviewCount}
+                location={val.location}
+                title={val.title}
+                price={val.price}
+              />
+    })
     return (
         <div>
             <Navbar />
             <Hero />
+            <section className="cards--list">
+              {card}
+            </section>
         </div>
     )
 }
--- src/components/Card.js
+++ src/components/Card.js
@@ -8,11 +8,11 @@ const Card = (props) => {
             <div className="card--stats">
                 <img src="../images/star.png" className="card--star" />
                 <span>{props.rating}</span>
-                <span className="gray">({props.reviewCount}) • </span>
+                <span className="gray">({props.reviewCount}) •  </span>
                 <span className="gray">{props.location}</span>
             </div>
-            <p>{props.title}</p>
-            <p><span className="bold">From ${props.price}</span> / person</p>
+            <p className="card--title">{props.title}</p>
+            <p><span className="bold card--price">From ${props.price}</span> / person</p>
         </div>
     )
 }
