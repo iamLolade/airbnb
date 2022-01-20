5c187d56525294f54a27bc437a08da297e80761c
iamLolade <adeniyiotemade@gmail.com>
2022-01-20 04:05:31 +0100

    Created and styled card component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react"
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
+import Card from "./components/Card";
 
 const App = () => {
     return (
